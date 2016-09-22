71bd18d488da28adfc2ed2616309033a5f451b92
rkhpe <ravikiran.basavaraj@hpe.com>
2016-09-22 10:43:08 +0000

    COmmiting Unit test cases

---
+++ unittest/test_01.py
@@ -0,0 +1,10 @@
+import unittest
+
+class SimplisticTest(unittest.TestCase):
+
+    def test(self):
+        print('Basic Unit Testing')
+        self.assertTrue(True)
+
+if __name__ == '__main__':
+    unittest.main()

49c631424192e0185b2c1e1662db7ee53f10adb9
rkhpe <ravikiran.basavaraj@hpe.com>
2016-10-19 10:41:51 +0000

    TEst

--- unittest/test_01.py
+++ unittest/test_01.py
@@ -4,6 +4,7 @@ class SimplisticTest(unittest.TestCase):
 
     def test(self):
         print('Basic Unit Test case demo')
+        print('Basic Unit 2 Test case demo')
         self.assertTrue(True)
 
 if __name__ == '__main__':

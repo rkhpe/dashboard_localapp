02d529bb1a2abb0ad1401c068b2fd1cb29af24b3
rkhpe <ravikiran.basavaraj@hpe.com>
2016-11-17 09:19:32 +0000

    Demo commit

--- unittest/test_01.py
+++ unittest/test_01.py
@@ -4,6 +4,7 @@ class SimplisticTest(unittest.TestCase):
 
     def test(self):
         print('Basic Unit Test case demo devops')
+        print('Basic Unit1 Test case demo devops')
         print('Basic Unit 2 Test case demo devops')
         self.assertTrue(True)
 
